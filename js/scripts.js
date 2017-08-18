0344283bdf2e0e0f582562945f13d010ad62cebf
ngottlieb87 <nogpdx@gmail.com>
2017-08-18 10:10:18 -0700

    add array to store integers of numver input and sort them. For loops to add ping, pong or return number in array

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,35 @@
   function pingPong(number){
-    for(var i=0; i<=number; i++){
-      if(number % 3 == 0){
-        alert("ping");
-      } else if(number % 5 === 0){
-        alert("pong");
-      } else if((number % 3 === 0) && (number % 5 === 0)){
-        alert("ping-pong");
+
+    indvNum=[];
+    numPingPong=[];
+    for(i = number; i > 0; i--){
+      indvNum.push(i);
+    }
+    indvNum= indvNum.sort(function(a,b){
+      return a-b;
+    });
+    for(var i=0; i<=indvNum[i]; i++){
+      if(indvNum[i] % 3 === 0 && indvNum[i] % 5 !== 0){
+        numPingPong.push("ping");
+      } else if(indvNum[i] % 5 === 0 && indvNum[i] % 3 !== 0){
+        numPingPong.push("pong");
+      }else if(indvNum[i] % 3 === 0 && indvNum[i] % 5 === 0){
+        numPingPong.push("ping-pong");
+      } else{
+        numPingPong.push(indvNum[i]);
       }
     }
+    listPingPong = numPingPong.join(" ");
+    return listPingPong;
   }
+
 $(document).ready(function(){
   $("#pingPong").submit(function(event){
     event.preventDefault();
 
     var number = parseInt($("#numInput").val());
     var result = pingPong(number);
-    $("#output").submit(result);
+
+    $("#output ul").show().text(result);
   })
 })
