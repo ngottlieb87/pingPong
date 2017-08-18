ea0919b83d5f2094d4422a27ed6287bae2b7de66
ngottlieb87 <nogpdx@gmail.com>
2017-08-18 10:20:39 -0700

    add a new array to push results from ping pong for loop and join them to string and return string

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,14 @@
 
     indvNum=[];
     numPingPong=[];
+
     for(i = number; i > 0; i--){
       indvNum.push(i);
     }
     indvNum= indvNum.sort(function(a,b){
       return a-b;
     });
+
     for(var i=0; i<=indvNum[i]; i++){
       if(indvNum[i] % 3 === 0 && indvNum[i] % 5 !== 0){
         numPingPong.push("ping");
@@ -20,16 +22,17 @@
       }
     }
     listPingPong = numPingPong.join(" ");
+
     return listPingPong;
   }
 
-$(document).ready(function(){
-  $("#pingPong").submit(function(event){
-    event.preventDefault();
+  $(document).ready(function(){
+    $("#pingPong").submit(function(event){
+      event.preventDefault();
 
-    var number = parseInt($("#numInput").val());
-    var result = pingPong(number);
+      var number = parseInt($("#numInput").val());
+      var result = pingPong(number);
 
-    $("#output ul").show().text(result);
-  })
-})
+      $("#output").show().text(result);
+    });
+  });
