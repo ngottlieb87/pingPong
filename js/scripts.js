7e0b838fc1284952eb1f93c80ea8c7fe93a3e1e8
ngottlieb87 <nogpdx@gmail.com>
2017-08-18 13:05:17 -0700

    add README.md and rules in html

--- js/scripts.js
+++ js/scripts.js
@@ -11,11 +11,11 @@
 
     for(var i=0; i<=indvNum[i]; i++){
       if(indvNum[i] % 3 === 0 && indvNum[i] % 5 !== 0){
-        numPingPong.push("ping");
+        numPingPong.push("Ping");
       } else if(indvNum[i] % 5 === 0 && indvNum[i] % 3 !== 0){
-        numPingPong.push("pong");
+        numPingPong.push("Pong");
       }else if(indvNum[i] % 3 === 0 && indvNum[i] % 5 === 0){
-        numPingPong.push("ping-pong");
+        numPingPong.push("Ping-Pong");
       } else{
         numPingPong.push(indvNum[i]);
       }
@@ -29,7 +29,7 @@
       event.preventDefault();
       var number = parseInt($("#numInput").val());
       var result = pingPong(number);
-        result.forEach(function(i){
+      result.forEach(function(i){
         $("#output ul").append("<li>" + i + "</li>");
         $("#output").show();
       });
