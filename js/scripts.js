6266c6c56be6ab56c59150fb677ef07b6be37437
ngottlieb87 <nogpdx@gmail.com>
2017-08-18 09:02:09 -0700

    add output field in html and for loops in js with if/else if statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,20 @@
-
-
-
-
-
-
-
-
-
+  function pingPong(number){
+    for(var i=0; i<=number; i++){
+      if(number % 3 == 0){
+        alert("ping");
+      } else if(number % 5 === 0){
+        alert("pong");
+      } else if((number % 3 === 0) && (number % 5 === 0)){
+        alert("ping-pong");
+      }
+    }
+  }
 $(document).ready(function(){
   $("#pingPong").submit(function(event){
     event.preventDefault();
 
-    number = parseInt($("#numInput").val());
+    var number = parseInt($("#numInput").val());
+    var result = pingPong(number);
+    $("#output").submit(result);
   })
 })
